eb688807a264f7c11f52828e8ddb889c3e4c0c25
Philipp Kosel <pkosel@fastmail.com>
2023-02-28 18:37:37 +0100

    Add some nvim keymaps

--- home/features/nvim/init.lua
+++ home/features/nvim/init.lua
@@ -19,6 +19,6 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
 
 require 'config.options'
--- require 'config.keymaps'
+require 'config.keymaps'
 
 vim.cmd [[colorscheme gruvbox-material]]
