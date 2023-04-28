6428e3caca2e26d12dd5802bc77230a2dddcef23
Philipp Kosel <170943+pkosel@users.noreply.github.com>
2023-04-28 16:40:05 +0200

    Move window-resizer extension to github

--- pkgs/window-resizer/default.nix
+++ pkgs/window-resizer/default.nix
@@ -1,47 +1,45 @@
 { lib
 , stdenv
-# , fetchFromGitHub
-# , gnome
-# , gettext
-# , glib
-, unzip
+, fetchFromGitHub
+, gnome
+, gettext
+, glib
 }:
 
 stdenv.mkDerivation rec {
   pname = "gnome-shell-extension-window-resizer";
   version = "1";
 
-  src = ./.;
-  #fetchFromGitHub {
-  #owner = "pkosel";
-  #repo = "window-resizer";
-  #rev = "";
-  #sha256 = "";
-  #};
+  src = fetchFromGitHub {
+    owner = "pkosel";
+    repo = "window-resizer";
+    rev = "aa3134adc32e31fb5006be51eb833042dcde0fb0";
+    sha256 = "sha256-5JP73QJyDDD7B2gRE1mcT4zi+2uCJZrM6kp1E+CJm7I=";
+  };
+
+  nativeBuildInputs = [ glib ];
+
+  buildPhase = ''
+    runHook preBuild
+    glib-compile-schemas --strict --targetdir=schemas schemas
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p "$out/share/gnome-shell/extensions/window-resizer@pkosel.github.com"
+    cp -r * "$out/share/gnome-shell/extensions/window-resizer@pkosel.github.com/"
+    runHook postInstall
+  '';
 
   passthru = {
     extensionUuid = "window-resizer@pkosel.github.com";
     extensionPortalSlug = "window-resizer";
   };
 
-  nativeBuildInputs = [ unzip ];
-
-  # buildPhase = ''
-  #   runHook preBuild
-  #   glib-compile-schemas --strict --targetdir=schemas schemas
-  #   runHook postBuild
-  # '';
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p "$out/share/gnome-shell/extensions/window-resizer@pkosel.github.com"
-    unzip -o "archive.zip" -d "$out/share/gnome-shell/extensions/window-resizer@pkosel.github.com"
-    runHook postInstall
-  '';
-
   meta = with lib; {
     description = "Programmatic window resizing for GNOME Shell";
-    license = licenses.gpl3;
+    license = licenses.gpl2;
     maintainers = with maintainers; [ pkosel ];
     homepage = "https://github.com/pkosel/window-resizer";
   };
