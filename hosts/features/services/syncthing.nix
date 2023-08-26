c945b52176a1ef7ffd86fafb1ceab97648fb9fc2
Philipp Kosel <170943+pkosel@users.noreply.github.com>
2023-08-26 18:27:20 +0200

    Update syncthing config

--- hosts/features/services/syncthing.nix
+++ hosts/features/services/syncthing.nix
@@ -8,18 +8,20 @@
     group = "users";
     overrideDevices = true;
     overrideFolders = true;
-    devices = {
-      "laptop" = {
-        id = "YNW3NRD-JGIMC7X-26MXMPI-OWPOK3P-ZPMPXFZ-4ZFK2YS-MXLAURW-SOVGQAI";
+    settings = {
+      devices = {
+        "laptop" = {
+          id = "YNW3NRD-JGIMC7X-26MXMPI-OWPOK3P-ZPMPXFZ-4ZFK2YS-MXLAURW-SOVGQAI";
+        };
+        "phone" = {
+          id = "OZGHQSB-YTYBBA5-3MHGSTP-6NDTKLQ-ZDTOFPO-WKS2NZY-26FENGZ-5EZQRQV";
+        };
       };
-      "phone" = {
-        id = "OZGHQSB-YTYBBA5-3MHGSTP-6NDTKLQ-ZDTOFPO-WKS2NZY-26FENGZ-5EZQRQV";
-      };
-    };
-    folders = {
-      "Syncthing" = {
-        path = "/home/philipp/Documents/Syncthing";
-        devices = [ "laptop" ];
+      folders = {
+        "Syncthing" = {
+          path = "/home/philipp/Documents/Syncthing";
+          devices = [ "laptop" ];
+        };
       };
     };
   };
